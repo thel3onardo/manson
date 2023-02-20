648c3a3c7ccbe49fab2d41a11d84f45b3766b339
thel3onardo <leonardo.contato03@gmail.com>
2023-02-20 08:40:41 -0300

    install tailwindcss, google-fonts, vue-use modules

---
+++ components/Insertion.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useMouseInElement } from '@vueuse/core'
+
+const target = ref(null)
+const { x, y, isOutside } = useMouseInElement(target)
+
+onMounted(() => {
+    console.log(x, y, isOutside)
+})
+
+watch(x, (val) => {
+    console.log(val)
+})
+</script>
+
+<template>
+    <p ref="target" class="text-red-500">Hello</p>
+</template>
