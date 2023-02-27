e1c6048d622c6782ce8bfd28c5a6be1bfa5fbe8a
thel3onardo <leonardo.contato03@gmail.com>
2023-02-27 20:10:19 -0300

    feat(web): add new gray variant value to tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,20 @@
 import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
-    theme: {
-    extend: {
-      fontFamily: {
-        manrope: ['Manrope', 'sans-serif']
+   theme: {
+      extend: {
+         fontFamily: {
+            manrope: ['Manrope', 'sans-serif'],
+         },
+         colors: {
+            orange: {
+               500: '#D87D4A',
+               200: '#FBAF85',
+            },
+            gray: {
+               100: '#F1F1F1',
+            },
+         },
       },
-      colors: {
-        orange: {
-            500: '#D87D4A',
-            200: '#FBAF85'
-        }
-      }
-    }
-  }
+   },
 }
