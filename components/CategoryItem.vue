13bc3318b0753449ed127be444b66884b810d046
thel3onardo <leonardo.contato03@gmail.com>
2023-02-28 11:13:46 -0300

    feat(web): start development of categoryItem component

---
+++ components/CategoryItem.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps({
+   imagePath: {
+      type: String,
+      required: true,
+   },
+   imageAlt: {
+      type: String,
+      required: true,
+   },
+   routePath: {
+      type: String,
+      required: true,
+   },
+   title: {
+      type: String,
+      required: true,
+   },
+})
+</script>
+
+<template>
+   <div
+      class="flex flex-col items-center bg-gray-200 font-manrope shadow rounded-lg"
+   >
+      <div class="h-[160px]">
+         <img :src="imagePath" :alt="imageAlt" class="h-full" />
+      </div>
+      <h3 class="font-bold text-lg tracking-widest uppercase">
+         {{ title }}
+      </h3>
+      <NuxtLink :to="routePath">
+         <span class="text-black text-sm tracking-wider opacity-50 mr-3"
+            >SHOP</span
+         >
+         <Icon
+            name="material-symbols:arrow-forward-ios"
+            class="text-orange-500"
+         />
+      </NuxtLink>
+   </div>
+</template>
