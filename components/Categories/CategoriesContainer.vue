a487918e641d9197141824cac8de99206485f671
thel3onardo <leonardo.contato03@gmail.com>
2023-03-18 06:49:10 -0300

    feat: develop categories component

---
+++ components/Categories/CategoriesContainer.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+interface ICategories {
+   id: number
+   name: string
+   routePath: string
+   imagePath: string
+   imageAlt: string
+}
+
+const categories: Array<ICategories> = [
+   {
+      id: 1,
+      name: 'headphones',
+      routePath: '/category/headphones',
+      imagePath: '/categories/image-category-thumbnail-headphones.png',
+      imageAlt: '',
+   },
+   {
+      id: 2,
+      name: 'headphones',
+      routePath: '/category/headphones',
+      imagePath: '/categories/image-category-thumbnail-speakers.png',
+      imageAlt: '',
+   },
+   {
+      id: 3,
+      name: 'earphones',
+      routePath: '/category/earphones',
+      imagePath: '/categories/image-category-thumbnail-earphones.png',
+      imageAlt: '',
+   },
+]
+</script>
+
+<template>
+   <div class="grid md:grid-cols-3 md:grid-rows-1 gap-x-8 my-40">
+      <CategoriesCategoryItem
+         v-for="category in categories"
+         :key="category.id"
+         :image-path="category.imagePath"
+         :image-alt="category.imageAlt"
+         :title="category.name"
+         :route-path="category.routePath"
+      />
+   </div>
+</template>
---
+++ components/Categories/CategoryItem.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps({
+   imagePath: {
+      type: String,
+      required: true,
+   },
+   imageAlt: {
+      type: String,
+      required: true,
+   },
+   routePath: {
+      type: String,
+      required: true,
+   },
+   title: {
+      type: String,
+      required: true,
+   },
+})
+</script>
+
+<template>
+   <div
+      class="flex flex-col justify-end items-center bg-gray-200 font-manrope shadow rounded-lg relative min-h-[204px] p-7 w-full"
+   >
+      <div class="h-[160px] absolute top-0 -translate-y-11">
+         <img :src="imagePath" :alt="imageAlt" class="h-full" />
+      </div>
+      <h3 class="font-bold text-lg tracking-widest uppercase mb-4">
+         {{ title }}
+      </h3>
+      <NuxtLink :to="routePath">
+         <span
+            class="text-black text-sm font-bold tracking-wider opacity-50 mr-3"
+            >SHOP</span
+         >
+         <Icon
+            name="material-symbols:arrow-forward-ios"
+            class="text-orange-500"
+         />
+      </NuxtLink>
+   </div>
+</template>
